35cc9526b953df776c057ab8da5388994398622b
Rodrigo dos Santos Felix <43767651+felixrodrigo19@users.noreply.github.com>
2020-04-21 15:33:48 -0300

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Node API
+
+[![license](https://img.shields.io/badge/license-Apache--2.0-green.svg)](https://tldrlegal.com/license/apache-license-2.0-(apache-2.0))
+![status](https://img.shields.io/badge/status-maintained-brightgreen.svg)
+
+
+## Usage
+### Installation
+```npm install```
+
+### Run
+```npm start```
---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/teste', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  console.log(err.status)
+});
+
+module.exports = app;
